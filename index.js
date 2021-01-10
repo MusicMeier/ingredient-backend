2f86321eb521acdc2fc9f695d1f432884fecfd61
Music <musicm987@gmail.com>
2021-01-09 20:51:19 -0700

    comparison function

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ const { createWorker } = Tesseract;
 
 const port = 7001;
 
+let string = "carrots, lima beans, blueberry, potatos, green bean, tomatos, avacados"
+let photoWords = "carrots, lima beans, apple, blueberry, strawberry"
+
+function compareStrings(string, photoWords){
+  let newString = string.split(',')
+  let updatedphotoWords = photoWords.split(',')
+  let matchingWords = newString.filter(element => updatedphotoWords.includes(element));
+  return matchingWords
+}
+
 app.set('view engine','ejs');
 // app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
@@ -56,13 +66,41 @@ app.post('/upload', upload, (request, response) => {
         await worker.initialize('eng');
         const { data: {text} } = await worker.recognize(image);
         console.log(text);
+        console.log(compareStrings(string, photoWords));
       })()
-      .then(result => {
-        response.send(result);
-      })
+      // .then(result => {
+      //   console.log(result)
+      // })
       // .finally(() => worker.terminate)
     });
   });
+
+// Array.prototype.diff = function(photoWords){
+//   let result = [];
+//   this.sort();
+//   photoWords.sort();
+//   for(var i=0; i < this.length; i++){
+//     if(photoWords.indexOf(this[i]) > -1){
+//       result.push(this[i])
+//     }
+//   }
+//   return result
+// }
+// console.log(newString.diff(updatedphotoWords))
+
+// function compareStrings(photoWords, string){
+//   let newString = string.split(',')
+//   let updatedphotoWords = photoWords.split(',')
+//   // let result = [];
+//   // for(i = 0; i < newString.length; i++ ){
+//   //   for(j=0; updatedphotoWords.length; j++){
+//   //     if (newString[i].equals(updatedphotoWords[j])){
+//   //       result = result.push(newString[i])
+//   //     }
+//   //   }
+//   // }
+//   return console.log(newString), console.log(updatedphotoWords)
+// }
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
